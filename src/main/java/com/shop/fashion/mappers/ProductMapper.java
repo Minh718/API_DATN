3c69bed52ff08ea9f2bee44484abd5cf1a8dc332
Minh718 <minhprokute85209@gmail.com>
2025-02-12 11:52:33 +0700

    setup entities and CSR for producct and complete func admin add product

---
+++ src/main/java/com/shop/fashion/mappers/ProductMapper.java
@@ -0,0 +1,25 @@
+package com.shop.fashion.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.shop.fashion.dtos.dtosRes.ProductDTO;
+import com.shop.fashion.entities.Product;
+
+@Mapper
+public interface ProductMapper {
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
+    @Mapping(target = "brand", ignore = true)
+    @Mapping(target = "categoryId", ignore = true)
+    @Mapping(target = "detailProduct", ignore = true)
+    @Mapping(target = "productSizes", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "subCategory", ignore = true)
+    @Mapping(target = "comments", ignore = true)
+    Product toProduct(ProductDTO productDTO);
+
+    @Mapping(source = "createdDate", target = "createdDate", dateFormat = "yyyy-MM-dd'T'HH:mm:ss")
+    ProductDTO toProductDTO(Product product);
+}
